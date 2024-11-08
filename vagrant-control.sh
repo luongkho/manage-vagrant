65ea44da8a0de725d66e8f81dcb3208a805a4a37
Luong Hoang <luongkhokhud@gmail.com>
2024-11-08 18:14:02 +0700

    Fix array substitution

--- vagrant-control.sh
+++ vagrant-control.sh
@@ -48,147 +48,147 @@ first_choice() {
     esac
 }
 
-#refresh_all() {
-#    for ((i=1; i<=count; i++)); do
-#        eval vagrant status \${id$i}
-#    done
-#    read_data
-#}
-#
-#shutdown_all() {
-#    for ((i=1; i<=count; i++)); do
-#        eval vagrant halt \${id$i}
-#    done
-#    read_data
-#}
-#
-#shutdown_computer() {
-#    for ((i=1; i<=count; i++)); do
-#        eval vagrant halt \${id$i}
-#    done
-#    echo
-#    echo "DANGER, PREPARE TO SHUTDOWN"
-#    read -t 10 -p "Press any key except [1] to cancel: " -n 1 -r
-#    if [[ $REPLY != 1 ]]; then
-#        echo "Shutdown canceled."
-#        read_data
-#    else
-#        shutdown -h +20s
-#    fi
-#}
-#
-#end() {
-#    exit 0
-#}
-#
-#choice_machine() {
-#    read -p "Machine: " choice
-#    eval id=\${id$choice}
-#    if [[ -z $id ]]; then
-#        choice_machine
-#    else
-#        machineId=$choice
-#        echo
-#        eval echo "Machine \${id$choice} \${status$choice} \${location$choice}"
-#        echo "1. Start machine"
-#        echo "2. SSH machine"
-#        echo "3. Restart machine"
-#        echo "4. Refresh"
-#        echo "5. Halt machine"
-#        echo "0. Back"
-#        choice_command
-#    fi
-#}
-#
-#choice_command() {
-#    read -p "Your choice: " choice
-#    case $choice in
-#        1) provision; start_machine ;;
-#        2) ssh_machine ;;
-#        3) provision; restart_machine ;;
-#        4) refresh_machine ;;
-#        5) halt_machine ;;
-#        0) main_menu ;;
-#        *) choice_command ;;
-#    esac
-#}
-#
-#provision() {
-#    echo
-#    echo "1. Normal boot"
-#    echo "2. Provision"
-#    read -p "Your choice: " provision
-#    case $provision in
-#        1) isProvision=0 ;;
-#        2) isProvision=1 ;;
-#        *) provision ;;
-#    esac
-#}
-#
-#refresh_machine() {
-#    refresh_command ${id$machineId}
-#    read_data
-#}
-#
-#start_machine() {
-#    start_command ${id$machineId}
-#    read_data
-#}
-#
-#halt_machine() {
-#    halt_command ${id$machineId}
-#    read_data
-#}
-#
-#ssh_machine() {
-#    get_machine_state ${id$machineId}
-#    if [[ $machineState == $STATE_RUNNING ]]; then
-#        ssh_command ${id$machineId}
-#    elif [[ $machineState == $STATE_OFF ]]; then
-#        echo "Try to start machine before ssh"
-#        start_command ${id$machineId}
-#        ssh_command ${id$machineId}
-#    else
-#        echo "Machine state is not valid: $machineState"
-#    fi
-#    read_data
-#}
-#
-#restart_machine() {
-#    reload_command ${id$machineId}
-#    read_data
-#}
-#
-#get_machine_state() {
-#    machineState=$(vagrant status $1 | awk 'NR==2 {print $2}')
-#}
-#
-#refresh_command() {
-#    vagrant status "$1"
-#}
-#
-#start_command() {
-#    if [[ $isProvision -eq 1 ]]; then
-#        vagrant up "$1" --provision
-#    else
-#        vagrant up "$1"
-#    fi
-#}
-#
-#reload_command() {
-#    if [[ $isProvision -eq 1 ]]; then
-#        vagrant reload "$1" --provision
-#    else
-#        vagrant reload "$1"
-#    fi
-#}
-#
-#ssh_command() {
-#    vagrant ssh "$1"
-#}
-#
-#halt_command() {
-#    vagrant halt "$1"
-#}
+refresh_all() {
+    for ((i=1; i<=count; i++)); do
+        eval vagrant status \${id$i}
+    done
+    read_data
+}
+
+shutdown_all() {
+    for ((i=1; i<=count; i++)); do
+        eval vagrant halt \${id$i}
+    done
+    read_data
+}
+
+shutdown_computer() {
+    for ((i=1; i<=count; i++)); do
+        eval vagrant halt \${id$i}
+    done
+    echo
+    echo "DANGER, PREPARE TO SHUTDOWN"
+    read -t 10 -p "Press any key except [1] to cancel: " -n 1 -r
+    if [[ $REPLY != 1 ]]; then
+        echo "Shutdown canceled."
+        read_data
+    else
+        shutdown -h +20s
+    fi
+}
+
+end() {
+    exit 0
+}
+
+choice_machine() {
+    read -p "Machine: " choice
+    eval id=\${id$choice}
+    if [[ -z $id ]]; then
+        choice_machine
+    else
+        machineId=$choice
+        echo
+        eval echo "Machine \${id$choice} \${status$choice} \${location$choice}"
+        echo "1. Start machine"
+        echo "2. SSH machine"
+        echo "3. Restart machine"
+        echo "4. Refresh"
+        echo "5. Halt machine"
+        echo "0. Back"
+        choice_command
+    fi
+}
+
+choice_command() {
+    read -p "Your choice: " choice
+    case $choice in
+        1) provision; start_machine ;;
+        2) ssh_machine ;;
+        3) provision; restart_machine ;;
+        4) refresh_machine ;;
+        5) halt_machine ;;
+        0) main_menu ;;
+        *) choice_command ;;
+    esac
+}
+
+provision() {
+    echo
+    echo "1. Normal boot"
+    echo "2. Provision"
+    read -p "Your choice: " provision
+    case $provision in
+        1) isProvision=0 ;;
+        2) isProvision=1 ;;
+        *) provision ;;
+    esac
+}
+
+refresh_machine() {
+    refresh_command "$id"
+    read_data
+}
+
+start_machine() {
+    start_command "$id"
+    read_data
+}
+
+halt_machine() {
+    halt_command "${id}"
+    read_data
+}
+
+ssh_machine() {
+    get_machine_state ${id}
+    if [[ $machineState == $STATE_RUNNING ]]; then
+        ssh_command ${id}
+    elif [[ $machineState == $STATE_OFF ]]; then
+        echo "Try to start machine before ssh"
+        start_command ${id}
+        ssh_command ${id}
+    else
+        echo "Machine state is not valid: $machineState"
+    fi
+    read_data
+}
+
+restart_machine() {
+    reload_command ${id}
+    read_data
+}
+
+get_machine_state() {
+    machineState=$(vagrant status $1 | awk 'NR==2 {print $2}')
+}
+
+refresh_command() {
+    vagrant status "$1"
+}
+
+start_command() {
+    if [[ $isProvision -eq 1 ]]; then
+        vagrant up "$1" --provision
+    else
+        vagrant up "$1"
+    fi
+}
+
+reload_command() {
+    if [[ $isProvision -eq 1 ]]; then
+        vagrant reload "$1" --provision
+    else
+        vagrant reload "$1"
+    fi
+}
+
+ssh_command() {
+    vagrant ssh "$1"
+}
+
+halt_command() {
+    vagrant halt "$1"
+}
 
 read_data
